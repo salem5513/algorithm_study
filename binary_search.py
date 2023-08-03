4b9a0f95092e3e4b22f06966defa3105040336f8
Anton <anton.ovchinnikov13@gmail.com>
2023-08-03 10:13:20 +0200

    bin s 4

--- binary_search.py
+++ binary_search.py
@@ -1,21 +1,20 @@
 list = [1, 3, 4, 6, 8]
-item = 4
+item = 8
 
 
-def bin_search(list, item):
+def bin_s(list, item):
     low = 0
     high = len(list) - 1
 
-    while low < high:
+    while low <= high:
         mid = (low + high) // 2
         guess = list[mid]
         if guess == item:
             return mid
-        elif guess > item:
+        elif guess >= item:
             high = mid - 1
         else:
             low = mid + 1
 
 
-print(bin_search(list, item))
-#that was third try
+print(bin_s(list, item))
